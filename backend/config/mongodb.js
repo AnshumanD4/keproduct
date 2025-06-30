8e4f284be4c0188cf43c3386a97bedcf2a4ba74a
anshumand4 <adiwakar19@gmail.com>
2025-06-30 20:56:34 +0530

    Initial commit

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("MongoDB connected ✅");
-  });
 
-  await mongoose.connect(`${process.env.MONGO_URI}/e-commerce`);
-};
+    mongoose.connection.on('connected',() => {
+        console.log("DB Connected");
+    })
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+
+}
 
 export default connectDB;
